44e44cb9fcf33feb6ded8063f33340d86e0def8f
Edwin Caro <stevencaro30@gmail.com>
2023-01-27 10:57:09 -0500

    Color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      'primaryFirst': '#008248',
+      'primarySecond': '#d4e9e2',
+      'secondaryFirst': '#f1f8f6',
+      'secondarySecond': '#f3f1e7',
+      'primaryText': '#1e3932',
+      'secondaryText': '#000000',
+    },
     extend: {},
   },
   plugins: [],
