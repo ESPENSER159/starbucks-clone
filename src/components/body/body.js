77e4f4e3f9c882d52d525bd8bb603f098e3d8b4c
Edwin Caro <stevencaro30@gmail.com>
2023-01-30 16:03:36 -0500

    Banner2

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -1,9 +1,15 @@
 import Banner1 from "./banner1"
+import Banner2 from "./banner2"
 
 export default function Body() {
     return (
-        <>
+        <div>
+            <div className="h-12 bg-primaryText text-white sticky font-bold pl-32 max-md:px-4 pr-10 flex items-center justify-between sticky top-0">
+                <h1>STARBUCKS REWARDS</h1>
+                <button className='text-sm border border-white font-semibold rounded-full px-4 py-2 font-mono hover:bg-primaryText hidden max-md:block'>Join in the app</button>
+            </div>
             <Banner1 />
-        </>
+            <Banner2 />
+        </div>
     )
 }
